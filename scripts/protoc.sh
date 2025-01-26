2764df1dbb60e243a195a7821ee407157698ebef
Jack Parsons <jparsons@ualberta.ca>
2025-01-25 22:49:13 -0700

    trigger

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -6,16 +6,16 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 for SERVICE_NAME in "order" "inventory" "payment"
 do
-protoc --go_out=./golang --go_opt=paths=source_relative \
-  --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+protoc --go_out=./proto/golang --go_opt=paths=source_relative \
+  --go-grpc_out=./proto/golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
 cd proto/golang/${SERVICE_NAME}
 go mod init \
-  github.com/jackparsonss/go-grpc/golang/${SERVICE_NAME} || true
+  github.com/jackparsonss/grpc-monorepo/proto/golang/${SERVICE_NAME} || true
 go mod tidy
+cd ../../../
 done
 
-cd ../../../
 git config --local user.name "github-actions[bot]"
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git add . && git commit -am "proto update" || true
