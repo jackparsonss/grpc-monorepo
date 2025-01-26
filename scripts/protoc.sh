1616fdb89bb1c8f68b2dabeb1768ab7d9de36097
Jack Parsons <jparsons@ualberta.ca>
2025-01-25 23:11:44 -0700

    test

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Fetch and checkout the branch explicitly
-git fetch origin $GITHUB_REF
-git checkout $GITHUB_REF
+git fetch origin $GITHUB_HEAD_REF
+git checkout $GITHUB_HEAD_REF
 
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
@@ -21,4 +21,4 @@ done
 git config --local user.name "github-actions[bot]"
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git add . && git commit -am "proto update" || true
-git push origin $GITHUB_REF
+git push origin $GITHUB_HEAD_REF
