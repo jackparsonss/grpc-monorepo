e60975489d324084d11f59418751cb85cb655b24
Jack Parsons <jparsons@ualberta.ca>
2025-01-26 13:58:14 -0700

    try docker

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -4,6 +4,10 @@
 git fetch origin $GITHUB_HEAD_REF
 git checkout $GITHUB_HEAD_REF
 
+echo "Current Directory: $(pwd)"
+ls -la
+git status
+
 cd proto
 
 for SERVICE_NAME in "order" "inventory" "payment"
@@ -13,6 +17,7 @@ protoc --go_out=./golang --go_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
 done
 
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --local user.name "github-actions[bot]"
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git add . && git commit -am "proto update" || true
