7ef11f2115a2ef97de85ba1ff7d0646dfed5944d
Jack Parsons <jparsons@ualberta.ca>
2025-01-25 22:45:15 -0700

    try again

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-SERVICE_NAME=$1
-RELEASE_VERSION=$2
 
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+for SERVICE_NAME in "order" "inventory" "payment"
+do
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
@@ -12,6 +13,8 @@ cd proto/golang/${SERVICE_NAME}
 go mod init \
   github.com/jackparsonss/go-grpc/golang/${SERVICE_NAME} || true
 go mod tidy
+done
+
 cd ../../../
 git config --local user.name "github-actions[bot]"
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
